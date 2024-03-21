8c3008652ed868827d1b5455169a85dd24745054
FrancescoCesari97 <freedus97@gmail.com>
2024-03-21 16:22:25 +0100

    aggiunti treni tramite ciclo foreach

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -15,20 +15,52 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        $train = new Train();
+        $trains_data = [
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Rimini',
+                'arrive_station' => 'Milano',
+                'departure_time' => '12:00:00',
+                'arrive_time' => '18:00:00',
+                'train_code' => 'x1s4d1ge',
+                'carriages' => 5,
+                'on_time' => 1,
+                'canceled' => 0,
+                'type' => 'Frecciabianca',
+                'highspeed' => 1,
+            ],
+            [
+                'company' => 'Altro',
+                'departure_station' => 'Torino',
+                'arrive_station' => 'Milano',
+                'departure_time' => '19:00:00',
+                'arrive_time' => '23:00:00',
+                'train_code' => 'x1s4d1ge',
+                'carriages' => 7,
+                'on_time' => 1,
+                'canceled' => 0,
+                'type' => 'Frecciarossa',
+                'highspeed' => 1,
+            ],
+        ];
 
-        $train->company = 'Trenitalia';
-        $train->departure_station = 'Rimini';
-        $train->arrive_station = 'Milano';
-        $train->departure_time = '12:00:00';
-        $train->arrive_time = '18:00:00';
-        $train->train_code = 'x1s4d1ge';
-        $train->carriages = 5;
-        $train->on_time = 1;
-        $train->canceled = 0;
-        $train->type = 'Frecciabianca';
-        $train->highspeed = 1;
+        foreach ($trains_data as $train_data) {
+            # code...
+            $train = new Train();
 
-        $train->save();
+            $train->company = $train_data['company'];
+            $train->departure_station = $train_data['departure_station'];
+            $train->arrive_station = $train_data['arrive_station'];
+            $train->departure_time = $train_data['departure_time'];
+            $train->arrive_time = $train_data['arrive_time'];
+            $train->train_code = $train_data['train_code'];
+            $train->carriages = $train_data['carriages'];
+            $train->on_time = $train_data['on_time'];
+            $train->canceled = $train_data['canceled'];
+            $train->type = $train_data['type'];
+            $train->highspeed = $train_data['highspeed'];
+
+            $train->save();
+        }
     }
 }
